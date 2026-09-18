067887d58ed36fefd1e03f186587a8004f740344
Jeffrey Tarog <jeffreycournerytarog@gmail.com>
2026-09-19 01:49:42 +0800

    DA-3 Tenant - Manage Tenant Domains

---
+++ Anexcs.Distro.Api/Contracts/Central/Tenant/CreateTenantDomainResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.Contracts.Central.Tenant;
+
+public class CreateTenantDomainResponse
+{
+    
+}
---
+++ Anexcs.Distro.Api/Contracts/Central/Tenant/CreateTenantResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Api.Contracts.Central;
+
+public record CreateTenantResponse(Guid Id);
