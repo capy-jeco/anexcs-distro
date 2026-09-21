587ba83d0b4b13fc9383ea365eba7f3eb9312a9f
Jeffrey Tarog <jeffreycournerytarog@gmail.com>
2026-09-21 23:51:27 +0800

    DA-6 - Tenancy - Provision Tenant Database

--- Anexcs.Distro.Domain/Entities/Central/Tenant.cs
+++ Anexcs.Distro.Domain/Entities/Central/Tenant.cs
@@ -1,9 +1,14 @@
-﻿namespace Anexcs.Distro.Domain.Entities.Central;
+﻿using Anexcs.Distro.Domain.Enums;
+
+namespace Anexcs.Distro.Domain.Entities.Central;
 
 public class Tenant
 {
     public Guid Id { get; private set; }
     public string Data { get; private set; } = "{}"; // JSON blob
+    public TenantStatus Status { get; private set; }
+    public string? DatabaseName { get; private set; }
+    public string? ServerKey { get; private set; }
     public DateTime CreatedAtUtc { get; private set; }
     public DateTime? UpdatedAtUtc { get; private set; }
 
@@ -16,5 +21,32 @@ public class Tenant
         Id = id;
         Data = data;
         CreatedAtUtc = DateTime.UtcNow;
+    }
+    
+    public void AssignDatabase(string databaseName, string serverKey)
+    {
+        DatabaseName = databaseName;
+        ServerKey = serverKey;
+        UpdatedAtUtc = DateTime.UtcNow;
+    }
+    
+    public void MarkAsProvisioning()
+    {
+        Status = TenantStatus.Provisioning;
+    }
+
+    public void Activate()
+    {
+        Status = TenantStatus.Active;
+    }
+
+    public void Suspend()
+    {
+        Status = TenantStatus.Suspended;
+    }
+
+    public void MarkProvisioningFailed()
+    {
+        Status = TenantStatus.ProvisioningFailed;
     }
 }
