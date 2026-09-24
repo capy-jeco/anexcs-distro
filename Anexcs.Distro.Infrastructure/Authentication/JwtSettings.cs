e4dddf7584e5767d2c5cf5c467972ad1783293c8
Jeffrey Tarog <jeffreycournerytarog@gmail.com>
2026-09-24 17:29:14 +0800

    DA-10 - Identity - Issue JWT Tokens

---
+++ Anexcs.Distro.Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,11 @@
+namespace Anexcs.Distro.Infrastructure.Authentication;
+
+public class JwtSettings
+{
+    public const string SectionName = "Jwt";
+
+    public string Secret { get; init; } = string.Empty;
+    public string Issuer { get; init; } = string.Empty;
+    public string Audience { get; init; } = string.Empty;
+    public int ExpiryMinutes { get; init; } = 60;
+}
