132939688ff2589d03a8ece897063b7e89d6d1ce
Jeffrey Tarog <jeffreycournerytarog@gmail.com>
2026-09-25 05:18:22 +0800

    DA-10 - Identity - Issue JWT Tokens

--- Anexcs.Distro.Infrastructure/Identity/CentralIdentityUser.cs
+++ Anexcs.Distro.Infrastructure/Identity/CentralIdentityUser.cs
@@ -2,11 +2,21 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Anexcs.Distro.Infrastructure.Identity;
 
-public class CentralIdentityUser : IdentityUser
+public class CentralIdentityUser : IdentityUser<Guid>
 {
     public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
 
     public string LastName { get; set; } = null!;
 
     public bool IsActive { get; set; } = true;
+
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    public DateTime? LastLoginAtUtc { get; set; }
+
+    public DateTime? EmailVerifiedAtUtc { get; set; }
 }
